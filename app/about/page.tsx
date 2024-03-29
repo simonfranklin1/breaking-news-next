0d2803a0029579b2c5fc11e6c09c0a78f760fd8f
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-03-29 15:24:36 -0300

    feat: Página de sobre nós

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,8 +16,15 @@ const page = () => {
                         Crie narrativas que ascendam a inspiração, conhecimento e entretenimento
                     </div>
                     <div className='text-xl text-gray-600'>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum nihil quis, aliquam modi iure alias consequuntur aperiam perferendis quod quae! Nam modi quibusdam laudantium dicta, earum qui explicabo! Maiores, explicabo!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, asperiores accusamus. Dolorum adipisci sapiente fugiat qui ipsa maxime doloremque architecto, sit veniam, similique nam deleniti nesciunt repellat et magnam repudiandae.
+                        Bem-vindo ao Breaking News, onde acreditamos que cada voz é importante.
+                        <br /><br />
+                        Breaking News não é apenas um site de notícias – é uma plataforma para as pessoas, pelas pessoas. Compreendemos o poder do conteúdo voltado para a comunidade, e é por isso que convidamos usuários como você a contribuir e compartilhar suas perspectivas sobre as histórias que mais importam para você.
+                        <br /><br />
+                        Quer você seja um jornalista experiente, um comentarista apaixonado ou simplesmente alguém com uma história para contar, o Breaking News oferece a oportunidade de criar e publicar suas próprias postagens. Nossa plataforma fácil de usar permite que os indivíduos compartilhem seus insights, experiências e opiniões, promovendo discussões e conexões significativas dentro de nossa comunidade.
+                        <br /><br />
+                        Na Breaking News, estamos comprometidos em promover a liberdade de expressão e promover uma gama diversificada de pontos de vista. Acreditamos que, ao capacitar os indivíduos para partilharem as suas vozes, podemos criar uma sociedade mais informada e inclusiva.
+                        <br /><br />
+                        Portanto, quer você esteja aqui para ler, escrever ou interagir com outras pessoas, damos as boas-vindas ao Breaking News – onde as notícias são feitas por você. Junte-se a nós para moldar a conversa e fazer sua voz ser ouvida. Juntos, vamos dar a notícia.
                     </div>
                 </div>
             </div>
