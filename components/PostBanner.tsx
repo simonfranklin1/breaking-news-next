43f0faf4c965f825f49b6bdf24a62be6e147c8eb
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-04-08 15:55:29 -0300

    refactor: Agora o banner de uma notícia aparece dependendo da sessão em que ele estiver

--- components/PostBanner.tsx
+++ components/PostBanner.tsx
@@ -6,12 +6,12 @@ import moment from 'moment';
 import "moment/locale/pt-br";
 import { TbPointFilled } from "react-icons/tb";
 
-const PostBanner = ({ post }: { post: NewsI }) => {
+const PostBanner = ({ post, rated }: { post: NewsI, rated?: boolean }) => {
     moment.locale("pt-br");
 
     return (
         <Link href={"/posts/" + post._id}>
-            <div style={{ backgroundImage: `url(${post.banner})` }} className="w-full rounded-xl lg:h-[530px] h-[400px] flex justify-start items-end p-4 px-8 text-white relative overflow-hidden banner-shadow zoom-div">
+            <div style={{ backgroundImage: `url(${post.banner})` }} className={`w-full rounded-xl lg:h-[530px] h-[400px] flex justify-start items-end p-4 px-8 text-white relative overflow-hidden banner-shadow ${rated ? "bg-[size:180%] bg-center hover:bg-[size:190%] duration-300" : "zoom-div"}`}>
                 <div className="flex flex-col gap-2 z-10">
                     <div className="flex items-center gap-3">
                         <img src={post.creator.avatar} alt="creator" className={`w-8 h-8" rounded-full object-cover`} />
@@ -19,18 +19,30 @@ const PostBanner = ({ post }: { post: NewsI }) => {
                             {post.creator.name} <span className="inline-block"><TbPointFilled /></span> {moment(post.createdAt).fromNow()}
                         </div>
                     </div>
-                    <div className="lg:text-3xl sm:text-xl hidden sm:block font-news font-semibold">
-                        {truncateText(post.title, 70)}
-                    </div>
-                    <div className="lg:text-lg sm:text-base hidden sm:block">
-                        {truncateText(post.text, 230)}
-                    </div>
-                    <div className="text-lg sm:hidden block font-news font-semibold">
-                        {truncateText(post.title, 30)}
-                    </div>
-                    <div className="text-base sm:hidden block">
-                        {truncateText(post.text, 70)}
-                    </div>
+
+                    {
+                        !rated && (
+                            <>
+                                <div className="lg:text-3xl sm:text-xl hidden sm:block font-news font-semibold">
+                                    {truncateText(post.title, 70)}
+                                </div>
+                                <div className="lg:text-lg sm:text-base hidden sm:block">
+                                    {truncateText(post.text, 230)}
+                                </div>
+                            </>
+                        ) || (
+                            <>
+                                <div className="lg:text-2xl sm:text-xl hidden sm:block font-news font-semibold">
+                                    {truncateText(post.title, 80)}
+                                </div>
+                                <div className="text-base hidden sm:block">
+                                    {truncateText(post.text, 110)}
+                                </div>
+
+                            </>
+                        )
+                    }
+
                     <div className="lg:text-lg sm:text-base capitalize font-medium">
                         {post.category}
                     </div>
--- components/RatedPosts.tsx
+++ components/RatedPosts.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { NewsI } from '@/types/types';
-import { PostCard } from '.';
+import { PostBanner, PostCard } from '.';
 import { truncateText } from '@/utils/utils';
 import Link from 'next/link';
 import RelatedPostCard from './RelatedPostCard';
@@ -20,31 +20,11 @@ const RatedPosts = ({ posts }: { posts: NewsI[] }) => {
             <div className="lg:grid lg:grid-cols-4 sm:grid-cols-2 gap-6 sm:h-[530px] h-[470px] hidden">
                 <PostCard key={posts[1]._id} {...posts[1]} />
                 <div className='col-span-2'>
-                    <Link href={"/posts/" + mostRated._id}>
-                        <div style={{ backgroundImage: `url(${mostRated.banner})` }} className='w-full rounded-xl lg:h-[530px] h-[400px] flex justify-start items-end p-4 px-8 text-white relative object-cover object-center overflow-hidden banner-shadow bg-[size:180%] bg-center hover:bg-[size:190%] duration-300'>
-                            <div className="flex flex-col gap-2">
-                                <div className="flex items-center gap-3">
-                                    <img src={mostRated.creator.avatar} alt="creator" className={`w-8 h-8" rounded-full object-cover`} />
-                                    <div>
-                                        {mostRated.creator.name} <span className="inline-block"><TbPointFilled /></span> {moment(mostRated.createdAt).fromNow()}
-                                    </div>
-                                </div>
-                                <div className="lg:text-2xl sm:text-xl hidden sm:block font-news font-semibold">
-                                    {truncateText(mostRated.title, 80)}
-                                </div>
-                                <div className="text-base hidden sm:block">
-                                    {truncateText(mostRated.text, 110)}
-                                </div>
-                                <div className="lg:text-lg sm:text-base capitalize font-medium">
-                                    {mostRated.category}
-                                </div>
-                            </div>
-                        </div>
-                    </Link>
+                    <PostBanner post={mostRated} rated />
                 </div>
                 <div className="flex flex-col">
-                    <RelatedPostCard post={posts[2]} rated={true} />
-                    <RelatedPostCard post={posts[3]} rated={true} />
+                    <RelatedPostCard post={posts[2]} rated />
+                    <RelatedPostCard post={posts[3]} rated />
                 </div>
             </div>
 
