c5b2c3f4d95d9d44ed3cee6148ef0170be28bce0
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-04-09 17:24:06 -0300

    refactor: Rota dos posts mais curtidos

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { NewsI } from '@/types/types'
-import { findLatestPosts } from '@/utils/utils';
+import { findLatestPosts, findRatedPosts } from '@/utils/utils';
 import React, { useEffect, useState } from 'react'
 import { LatestNews, Loading, RatedPosts, TopNews } from '.';
 import SportsNews from './SportsNews';
@@ -13,10 +13,8 @@ const Feed = () => {
     const latestPost = posts ? posts[0] : null;
 
     useEffect(() => {
-        findLatestPosts().then((response) => {
-            setPosts(response);
-            setRated(response.sort((a, b) => a.likes.length + b.likes.length));
-        });
+        findLatestPosts().then(response => setPosts(response));
+        findRatedPosts().then(response => setRated(response));
     }, [])
 
   return (
@@ -27,7 +25,7 @@ const Feed = () => {
                     <TopNews post={latestPost} />
                     <LatestNews posts={posts.filter((post) => post !== latestPost)} />
                     <SportsNews posts={posts.filter((post) => post.category === "esporte" && post !== latestPost)} />
-                    <RatedPosts posts={rated.filter((post) => post._id !== latestPost._id).slice(0, 4)} />
+                    <RatedPosts posts={rated.filter((post) => post._id !== latestPost._id).filter((post) => post._id !== latestPost._id).slice(0, 4)} />
                     <TopCreators />
                 </div>
             ) || (
