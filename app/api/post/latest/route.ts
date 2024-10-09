7d2e65b12f4aa34011d4451679055d513a3549e4
Simon Franklin <simonfranklin.54@gmail.com>
2024-10-09 20:26:37 -0300

    fix: Resgatando os dados do último post corretamente

--- app/api/post/latest/route.ts
+++ app/api/post/latest/route.ts
@@ -5,7 +5,7 @@ export const GET = async(req: Request) => {
     try {
         await connectToDataBase();
 
-        const latestPosts = await News.find().sort({ _id: -1 }).skip(1).limit(4).populate("creator");
+        const latestPosts = await News.find().sort({ _id: -1 }).limit(4).populate("creator");
 
         if(!latestPosts) {
             return new Response(JSON.stringify({ message: "Posts não encontrado!"}), { status: 404 });
