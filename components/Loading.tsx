be07d622bd1618f7bf6426392f968d0daacb813b
Simon Franklin <simonfranklin.54@gmail.com>
2025-04-15 15:08:10 -0300

    style: Melhorando exibição do componente de Loading e ajustando ao tamanho da tela do usuário

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ import { VscLoading } from "react-icons/vsc";
 
 const Loading = () => {
     return (
-        <div className="text-2xl text-center flex items-center min-h-[60vh] lg:min-h-[50vh]">
+        <div className="text-2xl text-center flex items-center min-h-[calc(100vh-88px)] lg:min-h-[calc(100vh-96px)]">
             <VscLoading className="animate-spin mx-auto" />
         </div>
     )
